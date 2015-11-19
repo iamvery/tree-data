9cf5a2d057973dc53e619c9e4534476d100fcb2f
Jay Hayes <ur@iamvery.com>
2015-11-19 09:24:17 -0600

    Fix things and stuff

--- tree.rb
+++ tree.rb
@@ -1,25 +1,40 @@
 Tree = Struct.new(:data) do
   def parse
-    #
+    # make the tests pass
   end
 end
 
 RSpec.describe Tree do
-  it "parses" do
-    data = [
-      { level: 1, sequence: 1 },
-      { level: 2, sequence: 2 },
-      { level: 3, sequence: 3 },
-      { level: 3, sequence: 4 },
-      { level: 2, sequence: 5 },
-      { level: 1, sequence: 6 },
-      { level: 2, sequence: 7 },
-      { level: 2, sequence: 8 },
-    ]
+  describe "#parse" do
+    it "parses single-level trees" do
+      data = [
+        { level: 1, sequence: 1 },
+        { level: 1, sequence: 2 },
+      ]
 
-    tree = described_class.new(data)
-    expect(tree.parse).to eq(
-      [
+      tree = described_class.new(data)
+
+      expect(tree.parse).to eq([
+        { sequence: 1, nodes: [] },
+        { sequence: 2, nodes: [] },
+      ])
+    end
+
+    it "parses complex tree data" do
+      data = [
+        { level: 1, sequence: 1 },
+        { level: 2, sequence: 2 },
+        { level: 3, sequence: 3 },
+        { level: 3, sequence: 4 },
+        { level: 2, sequence: 5 },
+        { level: 1, sequence: 6 },
+        { level: 2, sequence: 7 },
+        { level: 2, sequence: 8 },
+      ]
+
+      tree = described_class.new(data)
+
+      expect(tree.parse).to eq([
         {
           sequence: 1,
           nodes: [
@@ -37,10 +52,10 @@ RSpec.describe Tree do
           sequence: 6,
           nodes: [
             { sequence: 7, nodes: [] },
-            { sequence: 8, nodes: [] }
+            { sequence: 8, nodes: [] },
           ],
         }
-      ]
-    )
+      ])
+    end
   end
 end
