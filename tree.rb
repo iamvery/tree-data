441f25e6a7a42512f31796d84f700d7920aa693b
josh skeen <josh@joshskeen.com>
2015-11-20 13:30:29 -0500

    woops! put the test back how it was

--- tree.rb
+++ tree.rb
@@ -11,15 +11,17 @@ class Tree
     node[:nodes] = [] unless node.key?(:nodes)
     parent[:nodes] << node unless parent[:nodes].include?(node)
     return parent if @look_ahead.nil?
-    return if node[:level] > @look_ahead[:level]
-    next_node = @look_ahead
     if (node[:level] == @look_ahead[:level])
+      next_node = @look_ahead
       @look_ahead = @data.pop
       parse_node(parent, next_node)
     elsif node[:level] < @look_ahead[:level]
+      next_node = @look_ahead
       @look_ahead = @data.pop
       parse_node(node, next_node)
       parse_node(parent, node)
+    elsif node[:level] > @look_ahead[:level]
+      return
     end
     parent[:nodes]
   end
@@ -39,7 +41,6 @@ class Tree
       delete_level(x)
     end
   end
-
   RSpec.describe Tree do
     describe '#parse' do
       it 'parses single-level trees' do
@@ -60,33 +61,43 @@ class Tree
         data = [
           { level: 1, sequence: 1 },
           { level: 2, sequence: 2 },
-          { level: 3, sequence: 3 },
+          { level: 2, sequence: 3 },
           { level: 3, sequence: 4 },
-          { level: 2, sequence: 5 },
+          { level: 3, sequence: 5 },
           { level: 1, sequence: 6 },
           { level: 2, sequence: 7 },
-          { level: 2, sequence: 8 }
+          { level: 2, sequence: 8 },
+          { level: 3, sequence: 9 },
+          { level: 2, sequence: 10 }
         ]
+
         tree = described_class.new(data)
+
         expect(tree.parse).to eq([
           {
             sequence: 1,
             nodes: [
+              { sequence: 2, nodes: [] },
               {
-                sequence: 2,
+                sequence: 3,
                 nodes: [
-                  { sequence: 3, nodes: [] },
-                  { sequence: 4, nodes: [] }
+                  { sequence: 4, nodes: [] },
+                  { sequence: 5, nodes: [] }
                 ]
-              },
-              { sequence: 5, nodes: [] }
+              }
             ]
           },
           {
             sequence: 6,
             nodes: [
               { sequence: 7, nodes: [] },
-              { sequence: 8, nodes: [] }
+              {
+                sequence: 8,
+                nodes: [
+                  { sequence: 9, nodes: [] }
+                ]
+              },
+              { sequence: 10, nodes: [] }
             ]
           }
         ])
